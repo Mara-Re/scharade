4bef7cadca0abbf511e39067a6f4a58864a919af
Mara Reifenrath <mara.reifenrath@cashlink.de>
2021-01-07 09:50:30 +0100

    test styling final score view

--- src/components/FinalTeamScores.tsx
+++ src/components/FinalTeamScores.tsx
@@ -12,10 +12,10 @@ const useStyles = makeStyles({
         textAlign: "center",
         display: "flex",
         flexDirection: "row",
-        position: "relative"
+        position: "relative",
     },
     finalScoreColumn: {
-        width: "50%"
+        width: "50%",
     },
     winnerIndicator: {
         position: "absolute",
@@ -23,15 +23,32 @@ const useStyles = makeStyles({
         fontSize: "130px",
         // @ts-ignore
         zIndex: "-1",
-        marginTop: "42px"
+        marginTop: "42px",
     },
     winnerTeamEmoji: {
         fontSize: "50px",
-        marginTop: "-20px"
+        marginTop: "-20px",
     },
     score: {
-        fontSize: "56px"
-    }
+        fontSize: "56px",
+    },
+    test: {
+        position: "relative",
+        height: "80px",
+        width: "50px",
+        // margin: "auto"
+
+    },
+    testScore: {
+        position: "absolute",
+        textAlign: "center",
+        fontSize: "45px"
+    },
+    testWinnerIndicator: {
+        position: "absolute",
+        textAlign: "center",
+        fontSize: "80px"
+    },
 });
 
 interface FinalTeamScoresProps {
@@ -39,42 +56,49 @@ interface FinalTeamScoresProps {
 }
 
 const FinalTeamScores: FunctionComponent<FinalTeamScoresProps> = (props) => {
-    const {finalTeamScores} = props;
+    const { finalTeamScores } = props;
 
     const classes = useStyles();
 
     let winnerTeams: Team[] = [];
-    if (finalTeamScores[0].score === finalTeamScores[1].score ) {
-        winnerTeams = [finalTeamScores[0].team_a_or_b , finalTeamScores[1].team_a_or_b];
+    if (finalTeamScores[0].score === finalTeamScores[1].score) {
+        winnerTeams = [
+            finalTeamScores[0].team_a_or_b,
+            finalTeamScores[1].team_a_or_b,
+        ];
     } else if (finalTeamScores[0].score > finalTeamScores[1].score) {
-        winnerTeams = [finalTeamScores[0].team_a_or_b]
+        winnerTeams = [finalTeamScores[0].team_a_or_b];
     } else {
-        winnerTeams = [finalTeamScores[1].team_a_or_b]
+        winnerTeams = [finalTeamScores[1].team_a_or_b];
     }
 
     return (
-        <Box justifyContent='center' alignItems="center"
-             className={classes.finalScoresBox}
+        <Box
+            justifyContent="center"
+            alignItems="center"
+            className={classes.finalScoresBox}
         >
             {(finalTeamScores || []).map((team, index) => (
-                <div key={index}
-                     className={classes.finalScoreColumn}
-                >
-                    {winnerTeams.includes(team.team_a_or_b) && <div
-                        className={classes.winnerIndicator}
-                    >
-                        <div>🌟</div>
-                    </div>}
-                    <h1
-                        className={winnerTeams.includes(team.team_a_or_b) ? classes.winnerTeamEmoji : undefined}
-                    ><TeamEmoji team={team.team_a_or_b}/></h1>
-                    <h1
-                        className={classes.score}
-                    >{team.score}</h1>
+                <div key={index} className={classes.finalScoreColumn}>
+                    {/*{winnerTeams.includes(team.team_a_or_b) && <div*/}
+                    {/*    className={classes.winnerIndicator}*/}
+                    {/*>*/}
+                    {/*    <div>🌟</div>*/}
+                    {/*</div>}*/}
+                    <div className={classes.test}>
+                        <div className={classes.testWinnerIndicator}> 🌟</div>
+                        <div className={classes.testScore}>56</div>
+                    </div>
+                    {/*<h1*/}
+                    {/*    className={winnerTeams.includes(team.team_a_or_b) ? classes.winnerTeamEmoji : undefined}*/}
+                    {/*><TeamEmoji team={team.team_a_or_b}/></h1>*/}
+                    {/*<h1*/}
+                    {/*    className={classes.score}*/}
+                    {/*>56</h1>*/}
                 </div>
             ))}
         </Box>
     );
-}
+};
 
 export default FinalTeamScores;
